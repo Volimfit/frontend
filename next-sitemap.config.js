c73fc714cb0b14b54f88f9380156a84fdbc0d6e7
Sanya Domashnyi ^_^ <megamanok99@gmail.com>
2024-07-10 15:01:30 +0300

    recaptcha

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://volimfit.r',
+  siteUrl: process.env.SITE_URL || 'https://volimfit.ru',
   generateRobotsTxt: true, // опционально, можно отключить
   // ...другие опции
 }
--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -1,5 +1,6 @@
-import { Button, Input } from '@nextui-org/react';
+import { Button, Checkbox, Input } from '@nextui-org/react';
 import { useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import { useForm } from 'react-hook-form';
 
 export default function MyForm() {
@@ -7,15 +8,50 @@ export default function MyForm() {
   const [finish, setFinished] = useState(false);
   const [isLoading, setLoading] = useState(false); // Состояние для отслеживания загрузки
   const [isInvalid, setIsInvalid] = useState(false); // Состояние для отслеживания ошибки
-  const { register, handleSubmit } = useForm();
+  const [recaptchaValue, setRecaptchaValue] = useState(null); // Состояние для reCAPTCHA
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = async (data: any) => {
+    if (!recaptchaValue) {
+      alert('Пожалуйста, подтвердите, что вы не робот.');
+      return;
+    }
+
     setLoading(true); // Устанавливаем isLoading в true при отправке формы
 
     try {
       // Ваш код для отправки данных на сервер
-      console.log('Sending data:', data);
-
+      console.log(data);
+      // http://109.172.114.125:8111
+      const url = 'http://localhost:5000/mail/send-email';
+      const body = {
+        number: data.phone,
+        recaptcha: recaptchaValue,
+      };
+      console.log(JSON.stringify(body));
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log('Success:', data);
+        })
+        .catch((error) => {
+          console.error('There was a problem with the fetch operation:', error);
+        });
       // Можно добавить имитацию задержки для тестирования
       await new Promise((resolve) => setTimeout(resolve, 2000));
 
@@ -66,9 +102,16 @@ export default function MyForm() {
         onChange={handlePhoneChange}
         variant='bordered'
         fullWidth
-        errorMessage='Введите корректный номер телефона'
-        isInvalid={isInvalid} // Передаем состояние ошибки в компонент Input
+        errorMessage={isInvalid || errors.phone ? 'Введите корректный номер телефона' : ''}
+        isInvalid={isInvalid || !!errors.phone}
       />
+      <ReCAPTCHA
+        sitekey={`${process.env.RECAPTCHA_SECRET_KEY}`} // Замените на ваш site key
+        onChange={(value: any) => setRecaptchaValue(value)}
+      />
+      <Checkbox {...register('terms', { required: true })} isInvalid={!!errors.terms}>
+        Я согласен с правилами обработки персональных данных
+      </Checkbox>
       <Button className='w-full sm:w-auto' color='success' isLoading={isLoading} type='submit'>
         {isLoading ? 'Отправка...' : 'Оставить заявку'}
       </Button>
