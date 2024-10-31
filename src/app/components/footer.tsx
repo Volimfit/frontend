dbc908b0f5962236fa5929770d445894aba20bc1
Sanya Domashnyi ^_^ <megamanok99@gmail.com>
2024-10-31 17:30:53 +0300

    fix

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -31,6 +31,11 @@ export default function Footer() {
             <p className='mb-4'>
               <a href='#section'>сб-вс c 9:00 до 22:00</a>
             </p>
+            <Divider className=''/>
+            <div className='mb-4 flex justify-center font-semibold  md:justify-start'>
+              
+              <a  target='_blank' href="./oferta.pdf">Публичная оферта</a>
+            </div>
           </div>
 
           <div className='flex flex-col justify-center items-center sm:items-start  sm:justify-start'>
